bc8d746744220b4ef0bfe6103ae2dd639379303f
psotoa <55726682+psotoa@users.noreply.github.com>
2026-04-08 19:10:14 -0600

    Update hardhat.config.ts
    update para entrega 4

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "hardhat/config";
 import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const AMOY_RPC_URL = process.env.AMOY_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 export default defineConfig({
   solidity: {
@@ -9,6 +15,11 @@ export default defineConfig({
     localhost8546: {
       type: "http",
       url: "http://127.0.0.1:8546",
+    },
+    amoy: {
+      type: "http",
+      url: AMOY_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
   plugins: [hardhatToolboxMochaEthers],
